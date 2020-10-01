646aeeb6dbe911f4b5b409bbaad047ebf83e762e
Valentin-Gabriel Radu <valentin.radu@valinet.ro>
2020-10-02 02:08:29 +0300

    Added OS version check change to trigger symbol redownload. Fixed compilation on IA-32.

--- WinCenterTitleLibrary/dllmain.c
+++ WinCenterTitleLibrary/dllmain.c
@@ -10,6 +10,7 @@
 #define NUMBER_OF_REQUESTED_SYMBOLS         2
 #define DOWNLOAD_SYMBOLS_ERROR              0x100
 #define GET_SYMBOLS_ERROR                   0x101
+#define GET_OS_VERSION_ERROR                0x102
 #define SETTINGS_RELATIVE_PATH              "\\symbols\\settings.ini"
 #define SETTINGS_ADDRESSES_SECTION_NAME     "Addresses"
 #define MODULE_NAME_UDWM                    "uDWM"
@@ -17,6 +18,8 @@
 #define SYMBOL_NAME_MAX_LEN                 200
 #define ADDR_G_PDMINSTANCE                  "g_pdmInstance"
 #define ADDR_CDESKTOPMANAGER_LOADTHEME      "CDesktopManager::LoadTheme"
+#define SETTINGS_OS_SECTION_NAME            "OS"
+#define OS_VERSION_STRING                   "Build"
 
 #define SET_COLOR
 #undef SET_COLOR
@@ -30,6 +33,32 @@ funchook_t* funchook = NULL;
 HMODULE hModule = NULL;
 BYTE* titlebar_color = NULL;
 BYTE old_titlebar_color = 0;
+
+typedef LONG NTSTATUS, * PNTSTATUS;
+#define STATUS_SUCCESS (0x00000000)
+
+typedef NTSTATUS(WINAPI* RtlGetVersionPtr)(PRTL_OSVERSIONINFOW);
+
+BOOL GetOSVersion(PRTL_OSVERSIONINFOW lpRovi)
+{
+    HMODULE hMod = GetModuleHandleW(L"ntdll.dll");
+    if (hMod != NULL)
+    {
+        RtlGetVersionPtr fxPtr = (RtlGetVersionPtr)GetProcAddress(
+            hMod, 
+            "RtlGetVersion"
+        );
+        if (fxPtr != NULL)
+        {
+            lpRovi->dwOSVersionInfoSize = sizeof(RTL_OSVERSIONINFOW);
+            if (STATUS_SUCCESS == fxPtr(lpRovi)) 
+            {
+                return TRUE;
+            }
+        }
+    }
+    return FALSE;
+}
 
 static int(*DrawTextWFunc)(
     HDC     hdc,
@@ -173,7 +202,76 @@ __declspec(dllexport) DWORD WINAPI main(
                 0,
                 wszSettingsPath
             );
-            if (addresses[0] == 0 || addresses[1] == 0)
+
+            // https://stackoverflow.com/questions/36543301/detecting-windows-10-version/36543774#36543774
+            RTL_OSVERSIONINFOW rovi;
+            if (!GetOSVersion(&rovi))
+            {
+                FreeLibraryAndExitThread(
+                    hModule,
+                    GET_OS_VERSION_ERROR
+                );
+                return GET_OS_VERSION_ERROR;
+            }
+            // https://stackoverflow.com/questions/47926094/detecting-windows-10-os-build-minor-version
+            DWORD32 ubr = 0, ubr_size = sizeof(DWORD32);
+            HKEY hKey;
+            LONG lRes = RegOpenKeyExW(
+                HKEY_LOCAL_MACHINE,
+                wcschr(
+                    wcschr(
+                        wcschr(
+                            UNIFIEDBUILDREVISION_KEY, 
+                            '\\'
+                        ) + 1, 
+                        '\\'
+                    ) + 1, 
+                    '\\'
+                ) + 1,
+                0,
+                KEY_READ,
+                &hKey
+            );
+            if (lRes == ERROR_SUCCESS)
+            {
+                RegQueryValueExW(
+                    hKey,
+                    UNIFIEDBUILDREVISION_VALUE,
+                    0,
+                    NULL,
+                    &ubr,
+                    &ubr_size
+                );
+            }            
+            TCHAR szReportedVersion[_MAX_PATH];
+            ZeroMemory(
+                szReportedVersion,
+                (_MAX_PATH) * sizeof(TCHAR)
+            );
+            TCHAR szStoredVersion[_MAX_PATH];
+            ZeroMemory(
+                szStoredVersion,
+                (_MAX_PATH) * sizeof(TCHAR)
+            );
+            wsprintf(
+                szReportedVersion,
+                L"%d.%d.%d.%d",
+                rovi.dwMajorVersion,
+                rovi.dwMinorVersion,
+                rovi.dwBuildNumber,
+                ubr
+            );
+            VnGetString(
+                TEXT(SETTINGS_OS_SECTION_NAME),
+                TEXT(OS_VERSION_STRING),
+                szStoredVersion,
+                _MAX_PATH,
+                _MAX_PATH,
+                NULL,
+                wszSettingsPath
+            );
+
+            if (addresses[0] == 0 || addresses[1] == 0 || wcscmp(szReportedVersion, szStoredVersion))
             {
                 if (VnDownloadSymbols(
                     hModule,
@@ -211,6 +309,12 @@ __declspec(dllexport) DWORD WINAPI main(
                     TEXT(SETTINGS_ADDRESSES_SECTION_NAME),
                     TEXT(ADDR_CDESKTOPMANAGER_LOADTHEME),
                     addresses[1],
+                    wszSettingsPath
+                );
+                VnWriteString(
+                    TEXT(SETTINGS_OS_SECTION_NAME),
+                    TEXT(OS_VERSION_STRING),
+                    szReportedVersion,
                     wszSettingsPath
                 );
             }
