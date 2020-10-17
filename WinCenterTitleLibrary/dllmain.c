9567576bc7a375ce1c3de3d4748b4b1d9537027c
Valentin-Gabriel Radu <vali@valinet.ro>
2020-10-17 06:32:49 +0300

    Updated to new method which allows centering between window margins (instead of between icon, and window buttons), the same way it was in Windows 8; also, improved centering method to resemble more the way DWM worked in Windows 8; this should make this work well on Windows 7 as well, without blurring the whole title in Aero

--- WinCenterTitleLibrary/dllmain.c
+++ WinCenterTitleLibrary/dllmain.c
@@ -1,13 +1,14 @@
 #include <valinet/ini/ini.h>
 #include <valinet/pdb/pdb.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <Windows.h>
 #include <funchook.h>
 #pragma comment(lib, "Psapi.lib") //required by funchook
 
 #define MARGIN_OVERHANG_WIDTH_FIX           2
 #define AERO_COLOR_OFFSET                   0x19
-#define NUMBER_OF_REQUESTED_SYMBOLS         2
+#define NUMBER_OF_REQUESTED_SYMBOLS         5
 #define DOWNLOAD_SYMBOLS_ERROR              0x100
 #define GET_SYMBOLS_ERROR                   0x101
 #define GET_OS_VERSION_ERROR                0x102
@@ -18,6 +19,9 @@
 #define SYMBOL_NAME_MAX_LEN                 200
 #define ADDR_G_PDMINSTANCE                  "g_pdmInstance"
 #define ADDR_CDESKTOPMANAGER_LOADTHEME      "CDesktopManager::LoadTheme"
+#define ADDR_FUNC2                          "CMatrixTransformProxy::Update"
+#define ADDR_FUNC3                          "CText::ValidateResources"
+#define ADDR_FUNC4                          "CText::UpdateAlignmentTransform"
 #define SETTINGS_OS_SECTION_NAME            "OS"
 #define OS_VERSION_STRING                   "Build"
 
@@ -28,11 +32,6 @@
 #define TITLEBARS_WHITE_DEFAULT             1
 #define TITLEBARS_COLORED                   0
 #define TITLEBARS_DESIRED_COLOR             TITLEBARS_WHITE_DEFAULT
-
-funchook_t* funchook = NULL;
-HMODULE hModule = NULL;
-BYTE* titlebar_color = NULL;
-BYTE old_titlebar_color = 0;
 
 typedef LONG NTSTATUS, * PNTSTATUS;
 #define STATUS_SUCCESS (0x00000000)
@@ -45,13 +44,13 @@ BOOL GetOSVersion(PRTL_OSVERSIONINFOW lpRovi)
     if (hMod != NULL)
     {
         RtlGetVersionPtr fxPtr = (RtlGetVersionPtr)GetProcAddress(
-            hMod, 
+            hMod,
             "RtlGetVersion"
         );
         if (fxPtr != NULL)
         {
             lpRovi->dwOSVersionInfoSize = sizeof(RTL_OSVERSIONINFOW);
-            if (STATUS_SUCCESS == fxPtr(lpRovi)) 
+            if (STATUS_SUCCESS == fxPtr(lpRovi))
             {
                 return TRUE;
             }
@@ -60,6 +59,152 @@ BOOL GetOSVersion(PRTL_OSVERSIONINFOW lpRovi)
     return FALSE;
 }
 
+funchook_t* funchook = NULL;
+HMODULE hModule = NULL;
+BYTE* titlebar_color = NULL;
+BYTE old_titlebar_color = 0;
+
+typedef struct _milmatrix
+{
+    double s11;
+    double s12;
+    double s21;
+    double s22;
+    double dx;
+    double dy;
+} milmatrix;
+
+void* g_CTextInstance;
+
+static int64_t(*CTextUpdateAlignmentTransformFunc)(
+    void* _this
+    );
+
+static int64_t(*CTextValidateResourcesFunc)(
+    void* _this
+    );
+
+int64_t CTextValidateResourcesHook(
+    void* _this
+)
+{
+    int64_t rv;
+    DWORD* val;
+
+    // memorize address of this instance of CText, to be used in a subsequent
+    // call to CMatrixTransformProxy::Update on chain
+    // CText::ValidateResources - CText::UpdateAlignmentTransform
+    g_CTextInstance = _this;
+    // call original function
+    rv = CTextValidateResourcesFunc(_this);
+    // we need this hack so that we trick DWM into triggering an update of the
+    // title label at each resize, as now the title is centered so its
+    // position should change when the size of the window changes;
+    // how this works is that it seems DWM decides whether to update the
+    // label based on a check of whether the label size exceeded the window
+    // size; so, by setting the label size to a very large value, DWM is
+    // tricked into thinking the overflow is always happening
+    val = ((DWORD*)g_CTextInstance + 100);
+    if (*val < ENTIRE_TITLEBAR)
+    {
+        *val += ENTIRE_TITLEBAR;
+    }
+    return rv;
+}
+
+static int64_t(*CMatrixTransformProxyUpdateFunc)(
+    void* _this,
+    milmatrix* mat
+    );
+
+int64_t CMatrixTransformProxyUpdateHook(
+    void* _this,
+    milmatrix* mat
+)
+{
+    // this approach is based on how DWM centered the title bar text in
+    // Windows 8: instead of drawing the label from side to side with
+    // DT_CENTER, as we also did previously, DWM used to move the position
+    // where the label is rendered; although this was not necessary, it
+    // enabled the possiblity of Aero to work correctly (by not blurring the
+    // entire title bar, just the portion containing text), if Aero would
+    // have made a comeback
+
+    if (g_CTextInstance)
+    {
+        // this is the width of the title bar label containing the text
+        double v1 = *((DWORD*)g_CTextInstance + 100);
+        // this is the size available for the label (the size between the
+        // window icon, or the left window margin, and the caption buttons, or the
+        // right window margin)
+        double v3 = *((DWORD*)g_CTextInstance + 30);
+        // this is the height of the titlebar text (UNUSED)
+        double v4 = *((DWORD*)g_CTextInstance + 31);
+        // this is the width of the window icon
+        double v6 = *((DWORD*)g_CTextInstance + 32);
+        // this is the width of the window caption buttons (for example,
+        // Close, Minimize, Maximize etc)
+        double v5 = *((DWORD*)g_CTextInstance + 33);
+        // this also contains the width and height:
+        // tagSIZE st = *((tagSIZE*)g_CTextInstance + 15);
+        // where tagSIZE is a struct of 2 LONGs (width, and height)
+
+        // check if caller is CText::UpdateAlignmentTransfor
+        // do not hook CTextUpdateAlignmentTransform for this to work
+        // also, could be better improved by parsing the PE header
+        // or looking for the ret instruction of this code block;
+        // for now, for regular dwm compiles, it seems to work just fine like 
+        // this
+        if (
+            (-1) * (uintptr_t)_ReturnAddress() >
+            (-1) * (uintptr_t)CTextUpdateAlignmentTransformFunc - 120
+            )
+        {
+            // take into account previous hack; we need the real dimension
+            if (v1 > ENTIRE_TITLEBAR)
+            {
+                v1 = v1 - ENTIRE_TITLEBAR;
+            }
+            // we determine the "origin" of the label
+            double dx = (v3 + v5 - v6 - v1) / 2;
+            // this happens when the centered label overlaps the caption 
+            // buttons in that case, we center classically, between icon and
+            // caption buttons, same as Windows 8 did in this case
+            if (dx + v1 > v3)
+            {
+                dx = (v3 - v1) / 2;
+            }
+            // if there is not enough space to display the title, we left
+            // align,  and text ellipsis will be shown anyway
+            if (dx < 0)
+            {
+                mat->dx = 0;
+            }
+            else
+            {
+                mat->dx = dx;
+            }
+        }
+    }
+
+    // call original function
+    return CMatrixTransformProxyUpdateFunc(_this, mat);
+}
+
+static int64_t(*CDesktopManagerLoadThemeFunc)(
+    void* _this
+    );
+
+int64_t CDesktopManagerLoadThemeHook(
+    void* _this
+)
+{
+    int64_t ret = CDesktopManagerLoadThemeFunc(_this);
+    *titlebar_color = TITLEBARS_DESIRED_COLOR;
+    return ret;
+}
+
+/*
 static int(*DrawTextWFunc)(
     HDC     hdc,
     LPCWSTR lpchText,
@@ -99,19 +244,7 @@ int DrawTextWHook(
     }
     return rv;
 }
-
-static int64_t(*CDesktopManagerLoadThemeFunc)(
-    void* _this
-    );
-
-int64_t CDesktopManagerLoadThemeHook(
-    void* _this
-)
-{
-    int64_t ret = CDesktopManagerLoadThemeFunc(_this);
-    *titlebar_color = TITLEBARS_DESIRED_COLOR;
-    return ret;
-}
+*/
 
 __declspec(dllexport) DWORD WINAPI main(
     _In_ LPVOID lpParameter
@@ -134,6 +267,7 @@ __declspec(dllexport) DWORD WINAPI main(
     {
         funchook = funchook_create();
 
+        /*
         DrawTextWFunc = DrawTextW;
         rv = funchook_prepare(
             funchook, 
@@ -148,6 +282,7 @@ __declspec(dllexport) DWORD WINAPI main(
             );
             return rv;
         }
+        */
 
         // determine aero.msstyles code path flag location
         if (titlebar_color == NULL)
@@ -155,7 +290,10 @@ __declspec(dllexport) DWORD WINAPI main(
             SIZE_T dwRet;
             char* symbolNames[NUMBER_OF_REQUESTED_SYMBOLS] = {
                 ADDR_G_PDMINSTANCE, 
-                ADDR_CDESKTOPMANAGER_LOADTHEME 
+                ADDR_CDESKTOPMANAGER_LOADTHEME,
+                ADDR_FUNC2,
+                ADDR_FUNC3,
+                ADDR_FUNC4
             };
             DWORD addresses[NUMBER_OF_REQUESTED_SYMBOLS];
             ZeroMemory(
@@ -199,6 +337,24 @@ __declspec(dllexport) DWORD WINAPI main(
             addresses[1] = VnGetUInt(
                 TEXT(SETTINGS_ADDRESSES_SECTION_NAME),
                 TEXT(ADDR_CDESKTOPMANAGER_LOADTHEME),
+                0,
+                wszSettingsPath
+            );
+            addresses[2] = VnGetUInt(
+                TEXT(SETTINGS_ADDRESSES_SECTION_NAME),
+                TEXT(ADDR_FUNC2),
+                0,
+                wszSettingsPath
+            );
+            addresses[3] = VnGetUInt(
+                TEXT(SETTINGS_ADDRESSES_SECTION_NAME),
+                TEXT(ADDR_FUNC3),
+                0,
+                wszSettingsPath
+            );
+            addresses[4] = VnGetUInt(
+                TEXT(SETTINGS_ADDRESSES_SECTION_NAME),
+                TEXT(ADDR_FUNC4),
                 0,
                 wszSettingsPath
             );
@@ -271,7 +427,7 @@ __declspec(dllexport) DWORD WINAPI main(
                 wszSettingsPath
             );
 
-            if (addresses[0] == 0 || addresses[1] == 0 || wcscmp(szReportedVersion, szStoredVersion))
+            if (addresses[0] == 0 || addresses[1] == 0 || addresses[2] == 0 || wcscmp(szReportedVersion, szStoredVersion))
             {
                 if (VnDownloadSymbols(
                     hModule,
@@ -311,6 +467,24 @@ __declspec(dllexport) DWORD WINAPI main(
                     addresses[1],
                     wszSettingsPath
                 );
+                VnWriteUInt(
+                    TEXT(SETTINGS_ADDRESSES_SECTION_NAME),
+                    TEXT(ADDR_FUNC2),
+                    addresses[2],
+                    wszSettingsPath
+                );
+                VnWriteUInt(
+                    TEXT(SETTINGS_ADDRESSES_SECTION_NAME),
+                    TEXT(ADDR_FUNC3),
+                    addresses[3],
+                    wszSettingsPath
+                );
+                VnWriteUInt(
+                    TEXT(SETTINGS_ADDRESSES_SECTION_NAME),
+                    TEXT(ADDR_FUNC4),
+                    addresses[4],
+                    wszSettingsPath
+                );
                 VnWriteString(
                     TEXT(SETTINGS_OS_SECTION_NAME),
                     TEXT(OS_VERSION_STRING),
@@ -348,6 +522,47 @@ __declspec(dllexport) DWORD WINAPI main(
                 );
                 return rv;
             }
+
+            CMatrixTransformProxyUpdateFunc = (int64_t(*)(void*))(
+                (uintptr_t)hudwm +
+                (uintptr_t)addresses[2]
+                );
+            rv = funchook_prepare(
+                funchook,
+                (void**)&CMatrixTransformProxyUpdateFunc,
+                CMatrixTransformProxyUpdateHook
+            );
+            if (rv != 0)
+            {
+                FreeLibraryAndExitThread(
+                    hModule,
+                    rv
+                );
+                return rv;
+            }
+
+            CTextValidateResourcesFunc = (int64_t(*)(void*))(
+                (uintptr_t)hudwm +
+                (uintptr_t)addresses[3]
+                );
+            rv = funchook_prepare(
+                funchook,
+                (void**)&CTextValidateResourcesFunc,
+                CTextValidateResourcesHook
+            );
+            if (rv != 0)
+            {
+                FreeLibraryAndExitThread(
+                    hModule,
+                    rv
+                );
+                return rv;
+            }
+
+            CTextUpdateAlignmentTransformFunc = (int64_t(*)(void*))(
+                (uintptr_t)hudwm +
+                (uintptr_t)addresses[4]
+                );
         }
 
         rv = funchook_install(
